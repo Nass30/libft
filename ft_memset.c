6b64ff3bbb0995072e708e60b054622a1cd34046
Nassim BEN YAAGOUB <nben-yaa@e1r2p12.42.fr>
2018-06-25 11:10:21 +0200

    ADD

---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nben-yaa <nben-yaa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/22 15:37:41 by supasaitama       #+#    #+#             */
+/*   Updated: 2018/06/25 10:57:43 by nben-yaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	int				i;
+	unsigned char	*tmp;
+
+	tmp = (unsigned char *)b;
+	i = 0;
+	while (i < len)
+	{
+		tmp[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
